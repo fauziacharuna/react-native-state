f446dc19bcd51a09c560670b1c62c6f3b36abf38
Fauzi Achmad Haruna <fauziachmadharuna@192.168.1.32>
2022-07-11 23:40:08 +0800

    Adding fetch news api with axios

--- App.js
+++ App.js
@@ -6,58 +6,49 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
 
 export default function App() {
-    const [nilai, setNilai] = useState(0)
+    const [data, setData] = useState()
+    const getData = async () => {
+        try {
+            const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+                params: {
+                    country: 'us',
+                    category: 'business',
+                    apiKey: '1118dfb335664e10b4cb624816689b58'
+                }
+            })
+            setData(response.data.articles)
 
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
     return (
         <SafeAreaView>
-            <TouchableOpacity onPress={() => setNilai(nilai + 1)}>
-                <Tambah props={'Tambah'}/>
-            </TouchableOpacity>
-            <View>
-                <Text style={{fontSize: 40, color: 'black', alignSelf: 'center'}}>{nilai}</Text>
-            </View>
-            <TouchableOpacity onPress={() => setNilai(nilai - 1)}>
-                <Kurang props={'Kurang'}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setNilai(0)}>
-                <Reset props={'Reset'}/>
-            </TouchableOpacity>
+            <ScrollView>
+                {data && data.map((item, i) => {
+                    return (
+                        <>
+                            <View style={{flexDirection: 'row', marginVertical: 10, marginHorizontal: 5}}>
+                                <Image source={{uri: item.urlToImage}} style={{width: 100, height: 100}}></Image>
+                                <View style={{justifyContent:'space-between'}}>
+                                    <Text style={{marginHorizontal: 5, fontWeight:'bold'}}>{item.title}</Text>
+
+                                    <Text style={{marginHorizontal: 5}}>{item.author}</Text>
+                                </View>
+                            </View>
+                        </>
+                    )
+                })}
+            </ScrollView>
         </SafeAreaView>
-    )
-}
-
-export function Tambah({props}) {
-    return (
-        <View>
-            <Text style={{fontSize: 20, color: 'blue', alignSelf: 'center'}}>{props}</Text>
-        </View>
-    )
-}
-
-export function Kurang({props}) {
-    return (
-        <View>
-            <Text style={{fontSize: 20, color: 'blue', alignSelf: 'center'}}>{props}</Text>
-        </View>
-    )
-}
-
-export function Reset({props}) {
-    return (
-        <View>
-            <Text style={{fontSize: 20, color: 'blue', alignSelf: 'center'}}>{props}</Text>
-        </View>
-    )
-}
-
-export function Nilai() {
-    return (
-        <View>
-            <Text style={{fontSize: 40, color: 'black', alignSelf: 'center'}}></Text>
-        </View>
     )
 }
